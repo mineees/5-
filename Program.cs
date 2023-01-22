6d4e863072bdd3ab0e83ffbf688c157b0adf9839
mineees <minnullin09@gmail.com>
2023-01-22 20:58:50 +0300

    задача 34 5 урокока

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите массив");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Вот наш массив: ");
+PrintArray(numbers);
+int count = 0;
+
+for (int n = 0; n < numbers.Length; n++)
+if (numbers[n] % 2 == 0)
+count++;
+
+Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine();
+}
